603e7971a068b3f3dc658cf226ffe967c7ce19dc
Joy Das <35962760+joydas65@users.noreply.github.com>
2020-12-29 19:14:08 +0530

    Create Chef_And_Interactive_Contests.py

---
+++ Chef_And_Interactive_Contests.py
@@ -0,0 +1,7 @@
+# Question link https://www.codechef.com/problems/CHFINTRO
+
+n,r = map(int, input().split())
+
+for _ in range(n):
+        
+    print("Good boi" if int(input()) >= r else "Bad boi")
