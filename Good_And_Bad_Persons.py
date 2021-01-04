165778aa3e0e8b5a1bd50dfd3dc40aa02c8c6ea2
Joy Das <35962760+joydas65@users.noreply.github.com>
2021-01-04 12:25:40 +0530

    Create Good_And_Bad_Persons.py

---
+++ Good_And_Bad_Persons.py
@@ -0,0 +1,35 @@
+# Question Link https://www.codechef.com/problems/GOODBAD
+
+for _ in range(int(input())):
+    
+    n,k = map(int, input().split())
+    
+    s = input()
+    
+    uppercase,lowercase = 0,0
+    
+    for i in s:
+        
+        if i >= 'a' and i <= 'z':
+            
+            lowercase += 1
+            
+        elif i >= 'A' and i <= 'Z':
+            
+            uppercase += 1
+            
+    if uppercase <= k and lowercase <= k:
+        
+        print("both")
+        
+    elif uppercase <= k and lowercase > k:
+        
+        print("chef")
+        
+    elif uppercase > k and lowercase <= k:
+        
+        print("brother")
+        
+    else:
+        
+        print("none")
