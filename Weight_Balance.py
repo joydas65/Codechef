39c8f155818f4201bdfa252a11d62f4a4e88bee4
Joy Das <35962760+joydas65@users.noreply.github.com>
2021-03-26 10:53:23 +0530

    Create Weight_Balance.py

---
+++ Weight_Balance.py
@@ -0,0 +1,11 @@
+for _ in range(int(input())):
+    
+    w1,w2,x1,x2,m = map(int, input().split())
+    
+    if (w2-w1) >= (x1*m) and (w2-w1) <= (x2*m):
+        
+        print(1)
+        
+    else:
+        
+        print(0)
