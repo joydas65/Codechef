b8c91f5871a41da7259180dbc07496b768bcdb0b
Joy Das <35962760+joydas65@users.noreply.github.com>
2024-12-30 23:12:33 +0530

    Create Minimum_Deletions.py

---
+++ Minimum_Deletions.py
@@ -0,0 +1,17 @@
+def findGCD(a, b):
+    if a % b == 0:
+        return b
+    return findGCD(b, a % b)
+
+for _ in range(int(input())):
+    
+    n = int(input())
+    
+    arr = list(map(int, input().split()))
+    
+    gcd = arr[0]
+    
+    for i in range(1,n):
+        gcd = findGCD(gcd, arr[i])
+        
+    print(0 if gcd == 1 else -1)
